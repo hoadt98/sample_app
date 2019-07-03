56dd3f3871ad9aa2bd72a89c0935a112d6ce7b85
Hoa Duong <tahado98@gmail.com>
2019-07-03 16:02:57 +0700

    Update C6

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = valid_email_regax
+
   before_save {email.downcase!}
+
   validates :name, presence: true, length: { maximum: name_max }
   validates :email, presence: true, length: { maximum: email_max }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: pass_min }
+
   has_secure_password
 end
