9c7b87464b8d2ae5f838a91eb523914333a1febc
Hoa Duong <tahado98@gmail.com>
2019-07-03 08:24:33 +0700

    Updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'user/new'
-  root "static_pages#home"
-  get "/help", to: "static_pages#help"
-  get "/about", to: "static_pages#about"
-  get "/contact", to: "static_pages#contact"
-  get "/signup", to: "user#new"
+  scope "(:locale)", locale: /en|vi/ do
+  	get 'user/new'
+  	root "static_pages#home"
+  	get "/help", to: "static_pages#help"
+  	get "/about", to: "static_pages#about"
+  	get "/contact", to: "static_pages#contact"
+  	get "/signup", to: "user#new"
+  end
 end
