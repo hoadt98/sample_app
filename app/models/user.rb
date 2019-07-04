01b07aea96c90d3a609104f0d7b28e8d9e8ae8e2
Hoa Duong <tahado98@gmail.com>
2019-07-04 11:45:37 +0700

    Chapter 7

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
-  VALID_EMAIL_REGEX = valid_email_regax
+  VALID_EMAIL_REGEX = Settings.ver
 
   before_save {email.downcase!}
 
-  validates :name, presence: true, length: { maximum: name_max }
-  validates :email, presence: true, length: { maximum: email_max }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: pass_min }
+  validates :name, presence: true, length: { maximum: Settings.name_max }
+  validates :email, presence: true, length: { maximum: Settings.email_max }, format: { with: Settings.valid_email_regax }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: Settings.pass_min }
 
   has_secure_password
 end
