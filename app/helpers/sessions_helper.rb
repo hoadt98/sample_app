388dfe9eff78f824c12546dfb031fad0bd61161b
Hoa Duong <tahado98@gmail.com>
2019-07-08 15:08:02 +0700

    Update Chapter 8-2

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,8 +5,8 @@ module SessionsHelper
 
   def current_user
     return unless session[:user_id]
-      current_user
-      User.find_by(id: session[:user_id])
+    current_user
+    User.find_by id: session[:user_id]
   end
 
   def logged_in?
