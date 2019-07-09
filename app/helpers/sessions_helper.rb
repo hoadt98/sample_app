8d41a781c232d849edeb91e476eedd65843f336b
Hoa Duong <tahado98@gmail.com>
2019-07-09 10:45:20 +0700

    Update chapter 9-1

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,17 +11,16 @@ module SessionsHelper
 
   def forget user
     user.forget
-    cookies.delete(:user_id)
-    cookies.delete(:remember_token)
+    cookies.delete :user_id
+    cookies.delete :remember_token
   end
 
   def current_user
-    if (return unless session[:user_id]
-      current_user
-      User.find_by id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by id: user_id
     elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
+      user = User.find_by id: user_id
+      if user&.authenticated?(cookies[:remember_token])
         log_in user
         @current_user = user
       end
