f38912183d9a73cdc9271c86310a6cc6e78cdd4a
Hoa Duong <tahado98@gmail.com>
2019-07-10 11:17:30 +0700

    Chapter 10

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,11 +15,15 @@ module SessionsHelper
     cookies.delete :remember_token
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by id: user_id
-    elsif cookies.signed[:user_id]
-      user = User.find_by id: user_id
+    if user_id = session[:user_id]
+      @current_user ||= User.find_by(id: user_id)
+    elsif user_id = cookies.signed[:user_id]
+      user = User.find_by(id: user_id)
       if user&.authenticated?(cookies[:remember_token])
         log_in user
         @current_user = user
@@ -34,5 +38,14 @@ module SessionsHelper
   def log_out
     session.delete :user_id
     @current_user = nil
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: Settings.name_max }
   validates :email, presence: true, length: { maximum: Settings.email_max }, format: { with: Settings.valid_email_regax }, uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: Settings.pass_min }
+  validates :password, presence: true, length: { minimum: Settings.pass_min }, allow_nil: true
 
   has_secure_password
 
@@ -18,13 +18,13 @@ class User < ApplicationRecord
     end
 
     def new_token
-    SecureRandom.urlsafe_base64
+      SecureRandom.urlsafe_base64
     end
   end
 
   def remember
     self.remember_token = User.new_token
-    update :remember_digest: User.digest(remember_token)
+    update remember_digest: User.digest(remember_token)
   end
 
   def authenticated?(remember_token)
@@ -33,6 +33,6 @@ class User < ApplicationRecord
   end
 
   def forget
-    update :remember_digest: nil
+    update remember_digest: nil
   end
 end
