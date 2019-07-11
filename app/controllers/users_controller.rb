720de7eda724dde7134f1674c69053fa33f32eaa
Hoa Duong <tahado98@gmail.com>
2019-07-11 10:21:01 +0700

    Chapter 11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: %i(:index, :edit, :update, :destroy)
-  before_action :correct_user, only: %i(:edit, :update)
-  before_action :admin_user, only: %i(:destroy)
+  before_action :logged_in_user, only: %i(index edit update destroy)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: %i(destroy)
   before_action :load_user, only: %i(show edit update destroy)
 
   def index
-    @users = User.page params[:page]
+    @users = User.order(:user).page params[:page]
   end
 
   def show; end
@@ -17,9 +17,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      log_in @user
-      flash[:success] = t"welcome"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render :new
     end
